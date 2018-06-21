0770af9c371d8718947925033738c09d5dd4a996
John <john@hushkeys.com>
2018-06-21 12:26:48 +0100

    Initail commit with eslint setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './components/header';
+
+const App = () =>
+
+      <div className="App">
+        <Header />
+      </div>
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+const Header = () =>
+      <div className="header">
+        <ul className="menu">
+          <li>Info</li>
+          <li>Bilety</li>
+          <li>Kontak</li>
+        </ul>
+      </div>
+
+export default Header;
