d59b2cc0b00f45d7583110fbdb46b12ac9ecbc3c
John <john@hushkeys.com>
2018-07-03 11:32:20 +0100

    new images and slider tyle

--- src/components/elements/Slider.js
+++ src/components/elements/Slider.js
@@ -4,15 +4,19 @@ import './slider.css';
 import Img01 from '../../images/img01.png';
 import Img02 from '../../images/img02.png';
 import Img03 from '../../images/img03.png';
+import Img04 from '../../images/img04.jpg';
+import Img05 from '../../images/img05.jpg';
 
 class Slider extends React.Component {
   render() {
     return (
     <div className="slider">
-    <Carousel autoplay={true} width="550px" wrapAround={true}>
+    <Carousel autoplay={true} width="100vw" wrapAround={true} slidesToShow={3}>
       <img src={Img01} alt="img"/>
       <img src={Img02} alt="img"/>
       <img src={Img03} alt="img"/>
+      <img src={Img04} alt="img"/>
+      <img src={Img05} alt="img"/>
     </Carousel>
   </div>
 );
