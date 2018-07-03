8b5ba85efc7061c33de9ccf0c73eb121b59f0487
John <john@hushkeys.com>
2018-07-03 09:31:31 +0100

    ticket page css

--- src/components/pages/BiletyPage.js
+++ src/components/pages/BiletyPage.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import Subtn from '../../images/skiddleupBTN.png';
-import Sdbtn from '../../images/skiddledownBTN.png';
-import Eubtn from '../../images/EBupBTN.png';
-import Edbtn from '../../images/EBdownBTN.png';
 import './bilety.css';
 
 class BiletyPage extends React.Component {
@@ -10,22 +6,10 @@ class BiletyPage extends React.Component {
     return (
       <div>
         <a href="https://www.skiddle.com/e/13237502">
-          <img
-            className="skiddle"
-            src={Subtn}
-            atl="Skiddle"
-            onMouseOver={e => (e.currentTarget.src = {Sdbtn})}
-            onMouseOut={e => (e.currentTarget.src = {Subtn})}
-          />
+          <div className="skiddle" alt="img"/>
         </a>
         <a href="https://www.eventbrite.co.uk/e/tajemniczy-smak-lata-tickets-46566961034">
-          <img
-            className="eventbrite"
-            src={Eubtn}
-            atl="Eventbrite"
-            onMouseOver={e => (e.currentTarget.src = {Edbtn})}
-            onMouseOut={e => (e.currentTarget.src = {Eubtn})}
-          />
+          <div className="eventbrite" alt="img"/>
         </a>
       </div>
     );
